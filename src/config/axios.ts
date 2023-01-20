6201d72135215647056b905b50b75e09ec1fed49
Farhan Jamil <farhanjamil259@gmail.com>
2023-01-20 15:31:21 +0500

    fix: token in header fix

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -5,12 +5,8 @@ const token = localStorage.getItem("token");
 
 export const axiosPrivate = axios.create({
   baseURL: BASE_URL,
-});
-
-axiosPrivate.interceptors.request.use((config: any) => {
-  if (config.headers) {
-    config.headers["Accept"] = "application/json";
-    config.headers["Authorization"] = token ? `Bearer ${localStorage.getItem("token")}` : undefined;
-  }
-  return config;
+  headers: {
+    Accept: "application/json",
+    Authorization: token ? `Bearer ${localStorage.getItem("token")}` : undefined,
+  },
 });
