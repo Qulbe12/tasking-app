b77c7fdd6a4c888b0970516d23e2a78baaa2bdd4
Farhan Jamil <farhanjamil259@gmail.com>
2023-01-26 06:00:45 +0500

    chore: updated base url

--- src/constants/URLS.ts
+++ src/constants/URLS.ts
@@ -1,5 +1,5 @@
-// export const BASE_URL = "https://api.hexadesk.io/api";
-export const BASE_URL = "http://localhost:8000/api";
+export const BASE_URL = "https://api.hexadesk.io/api";
+// export const BASE_URL = "http://localhost:8000/api";
 
 export const LOGIN_ROUTE = "/users/login";
 export const REGISTER_ROUTE = "/users/register";
