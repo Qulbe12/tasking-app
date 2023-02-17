a64f47a26c7f3875a70ddd1772e631db5e47834b
Farhan Jamil <farhanjamil259@gmail.com>
2023-02-17 07:17:14 +0500

    chore: update api url to new url

--- src/constants/URLS.ts
+++ src/constants/URLS.ts
@@ -1,6 +1,5 @@
 // export const BASE_URL = "https://api.hexadesk.io/api";
-export const BASE_URL =
-  "https://hexadesknestbe-env.eba-bmhfgnad.us-east-1.elasticbeanstalk.com/api";
+export const BASE_URL = "https://api.hexadesk.ca/api";
 // export const BASE_URL = "http://localhost:8000/api";
 // export const BASE_URL = "http://192.168.1.19:8000/api";
 
