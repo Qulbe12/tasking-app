86dcdacf44b1b2cfa15c6a8dde5b6cc49adf7a6c
Farhan Jamil <farhanjamil259@gmail.com>
2023-08-01 15:45:06 +0500

    fix: permissions

--- src/layouts/SettingsLayout.tsx
+++ src/layouts/SettingsLayout.tsx
@@ -1,11 +1,19 @@
 import { Container, Grid, NavLink } from "@mantine/core";
 import { IconBuilding, IconCards, IconUser } from "@tabler/icons";
 import { Outlet, useLocation, useNavigate } from "react-router-dom";
+import { useAppSelector } from "../redux/store";
+import { useMemo } from "react";
 
 const SettingsLayout = () => {
   const navigate = useNavigate();
 
   const location = useLocation();
+
+  const { user } = useAppSelector((state) => state.auth);
+
+  const isOwner = useMemo(() => {
+    return user?.user.business.isOwner;
+  }, [user]);
 
   const onLinkClick = (to: string) => {
     navigate(`/account/settings/${to}`);
@@ -31,24 +39,28 @@ const SettingsLayout = () => {
               }}
             />
           </NavLink>
-          <NavLink icon={<IconCards />} label="Billing and Plans">
-            <NavLink
-              active={location.pathname === "/account/settings/payment-methods"}
-              label="Payment Methods"
-              onClick={() => {
-                onLinkClick("payment-methods");
-              }}
-            />
-          </NavLink>
-          <NavLink icon={<IconBuilding />} label="Business Management">
-            <NavLink
-              active={location.pathname === "/account/settings/manage-seats"}
-              label="Manage Seats"
-              onClick={() => {
-                onLinkClick("manage-seats");
-              }}
-            />
-          </NavLink>
+          {isOwner && (
+            <NavLink icon={<IconCards />} label="Billing and Plans">
+              <NavLink
+                active={location.pathname === "/account/settings/payment-methods"}
+                label="Payment Methods"
+                onClick={() => {
+                  onLinkClick("payment-methods");
+                }}
+              />
+            </NavLink>
+          )}
+          {isOwner && (
+            <NavLink icon={<IconBuilding />} label="Business Management">
+              <NavLink
+                active={location.pathname === "/account/settings/manage-seats"}
+                label="Manage Seats"
+                onClick={() => {
+                  onLinkClick("manage-seats");
+                }}
+              />
+            </NavLink>
+          )}
         </Grid.Col>
         <Grid.Col span={9}>
           <Outlet />
