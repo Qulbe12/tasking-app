53d9e091c726cbd0e66c67b40fd1f067b3177e5f
Farhan Jamil <farhanjamil259@gmail.com>
2023-09-30 03:15:33 +0500

    chore: test commented

--- src/components/ProcessSheet.tsx
+++ src/components/ProcessSheet.tsx
@@ -1,61 +1,7 @@
-import React, { useState } from "react";
-import SheetModal from "../modals/SheetModal";
-import { Affix, Notification, Stack } from "@mantine/core";
-import { useAppDispatch, useAppSelector } from "../redux/store";
-import { initSheetRecords } from "../redux/slices/sheetSlice";
+import React from "react";
 
 const ProcessSheet = () => {
-  const dispatch = useAppDispatch();
-  const [showModal, setShowModal] = useState(false);
-  const {
-    loaders: { processingSheet },
-    sheetRecords,
-  } = useAppSelector((state) => state.sheets);
-
-  return (
-    <div>
-      <Affix position={{ bottom: 20, right: 20 }}>
-        <Stack>
-          {processingSheet && (
-            <Notification
-              onClick={() => {
-                if (!processingSheet) {
-                  setShowModal(true);
-                }
-              }}
-              disallowClose={true}
-              title={"Processing Sheet"}
-              loading={processingSheet}
-            />
-          )}
-
-          {!showModal && sheetRecords.length > 0 && (
-            <Notification
-              className="hover:cursor-pointer"
-              onClick={() => {
-                if (!processingSheet) {
-                  setShowModal(true);
-                }
-              }}
-              disallowClose={false}
-              title={"Sheet processed."}
-              loading={processingSheet}
-            >
-              Sheet successfully processed, please click here to create it.
-            </Notification>
-          )}
-        </Stack>
-      </Affix>
-
-      <SheetModal
-        opened={showModal}
-        onClose={() => {
-          setShowModal(false);
-          dispatch(initSheetRecords());
-        }}
-      />
-    </div>
-  );
+  return <div>ProcessSheet</div>;
 };
 
 export default ProcessSheet;
