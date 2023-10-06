13208cfecf0a6a21cdf3a6439e5dcdb16c8c9345
Farhan Jamil <farhanjamil259@gmail.com>
2023-10-06 15:04:53 +0500

    fix: build fix

--- src/components/CustomTextEditor.tsx
+++ src/components/CustomTextEditor.tsx
@@ -5,6 +5,8 @@ import StarterKit from "@tiptap/starter-kit";
 import Underline from "@tiptap/extension-underline";
 import TextAlign from "@tiptap/extension-text-align";
 import Placeholder from "@tiptap/extension-placeholder";
+import { Color } from "@tiptap/extension-color";
+import TextStyle from "@tiptap/extension-text-style";
 import { ActionIcon, Group, Menu } from "@mantine/core";
 import { IconSettings, IconSignature } from "@tabler/icons";
 import { useAppSelector } from "../redux/store";
@@ -41,6 +43,8 @@ export default function CustomTextEditor({
         Underline,
         Link,
         Highlight,
+        TextStyle,
+        Color,
         TextAlign.configure({ types: ["heading", "paragraph"] }),
         Placeholder.configure({ placeholder: "Compose your message" }),
       ],
@@ -71,6 +75,24 @@ export default function CustomTextEditor({
             <RichTextEditor.Underline />
             <RichTextEditor.Strikethrough />
             <RichTextEditor.Highlight />
+            <RichTextEditor.ColorPicker
+              colors={[
+                "#25262b",
+                "#868e96",
+                "#fa5252",
+                "#e64980",
+                "#be4bdb",
+                "#7950f2",
+                "#4c6ef5",
+                "#228be6",
+                "#15aabf",
+                "#12b886",
+                "#40c057",
+                "#82c91e",
+                "#fab005",
+                "#fd7e14",
+              ]}
+            />
           </RichTextEditor.ControlsGroup>
 
           <RichTextEditor.ControlsGroup>
@@ -80,24 +102,28 @@ export default function CustomTextEditor({
             <RichTextEditor.H4 />
           </RichTextEditor.ControlsGroup>
 
-          <RichTextEditor.ControlsGroup>
-            <RichTextEditor.Blockquote />
-            <RichTextEditor.Hr />
-            <RichTextEditor.BulletList />
-            <RichTextEditor.OrderedList />
-          </RichTextEditor.ControlsGroup>
+          {!isSignature && (
+            <>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Blockquote />
+                <RichTextEditor.Hr />
+                <RichTextEditor.BulletList />
+                <RichTextEditor.OrderedList />
+              </RichTextEditor.ControlsGroup>
 
-          <RichTextEditor.ControlsGroup>
-            <RichTextEditor.Link />
-            <RichTextEditor.Unlink />
-          </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.Link />
+                <RichTextEditor.Unlink />
+              </RichTextEditor.ControlsGroup>
 
-          <RichTextEditor.ControlsGroup>
-            <RichTextEditor.AlignLeft />
-            <RichTextEditor.AlignCenter />
-            <RichTextEditor.AlignJustify />
-            <RichTextEditor.AlignRight />
-          </RichTextEditor.ControlsGroup>
+              <RichTextEditor.ControlsGroup>
+                <RichTextEditor.AlignLeft />
+                <RichTextEditor.AlignCenter />
+                <RichTextEditor.AlignJustify />
+                <RichTextEditor.AlignRight />
+              </RichTextEditor.ControlsGroup>
+            </>
+          )}
           {!isSignature && (
             <Group position="right" m="md">
               <Menu>
