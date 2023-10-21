74b33c6b36dde19779b9f54ca2d1d75bd937d54d
Farhan Jamil <farhanjamil259@gmail.com>
2023-10-21 22:27:02 +0500

    chore: updated to use ssl

--- src/constants/URLS.ts
+++ src/constants/URLS.ts
@@ -3,7 +3,7 @@ const API_URL = "https://api.hexadesk.ca";
 export const BASE_URL = `${API_URL}/api`;
 export const SOCKET_URL = "https://sockets.hexadesk.ca";
 
-export const SHEETS_URL = "http://sheets-service-v3.hexadesk.ca";
+export const SHEETS_URL = "https://sheets-service-v3.hexadesk.ca";
 
 export const LOGIN_ROUTE = "/users/login";
 export const REGISTER_ROUTE = "/users/register";
